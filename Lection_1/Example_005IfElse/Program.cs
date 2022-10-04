51b0eec5c6f17e6930327addcdff5d5436493a48
Artiom30 <mezheynikov1@gmail.com>
2022-10-04 18:51:25 +0300

    Favorite user

---
+++ Lection_1/Example_005IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hi. Imput yuor name :");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура , это же Маша!!!");
+}
+else 
+{
+    Console.WriteLine("hello ," + username);
+}
