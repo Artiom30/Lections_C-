00f470d5bdf95ceed2f57b143cb472535b8c6708
Artiom30 <mezheynikov1@gmail.com>
2022-10-09 09:32:39 +0300

    Example 6

---
+++ Lection_1/Example_006Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;           //   
+int b = 2;           //   
+int c = 6;           //     Ввод данных
+int d = 8;           //       
+int e = 4;           //       
+
+int max = a;         //     Временная переменная
+
+if ( a > max ) max = a;     //
+if ( b > max ) max = b;     //
+if ( c > max ) max = c;     //    Тело программы... 
+if ( d > max ) max = d;     //
+if ( e > max ) max = e;     //
+
+Console.WriteLine("max = " + max);      //  Вывод результата
