4f83cb56a7beb8dcc8ab86951128a2149058f691
Artiom30 <mezheynikov1@gmail.com>
2022-11-09 21:12:39 +0300

    Lection 7 Task1

--- Lection_7/Task1.cs
+++ Lection_7/Task1.cs
@@ -1,1 +1,25 @@
-﻿//
+﻿// Задача 1.
+// Собрать строку с числами от a до b ( a <= b)
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = b; i >= a; i--)
+        result += $"{i} ";
+    return result;
+}
+// Обычный метод
+
+string NumbersRes(int a, int b)
+{
+    if (a <= b) return NumbersRes(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+// Рекурсивный метод.
+Console.WriteLine(NumbersFor(1, 10)); //1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumbersRes(1, 10)); //1 2 3 4 5 6 7 8 9 10
+
+// Чтобы поменять заполнение строки от большего к меньшему:
+// for (int i = a; i >= b; i--) в первом случае изменим условие.
+// а во втором поменяем возвращаемы результат : Сначала метод затем
+// строка заполнения...
