c69dae43e1c710c1f1dfaa934b9f33e43293fb7a
Artiom30 <mezheynikov1@gmail.com>
2022-11-15 19:00:49 +0300

    Task 1

--- Lection_7/Task1.cs
+++ Lection_7/Task1.cs
@@ -4,17 +4,27 @@
 string NumbersFor(int a, int b)
 {
     string result = String.Empty;
-    for (int i = b; i >= a; i--)
+    for (int i = a; i <= b; i++)
         result += $"{i} ";
     return result;
 }
+// { для обратного заполнения....
+//     string result = String.Empty;
+//     for (int i = b; i >= a; i--)
+//         result += $"{i} ";
+//     return result;
+// }
 // Обычный метод
 
 string NumbersRes(int a, int b)
 {
-    if (a <= b) return NumbersRes(a + 1, b) + $"{a} ";
+    if (a <= b) return  $"{a} " + NumbersRes(a + 1, b)";
     else return String.Empty;
 }
+// { для обратного заполнения`
+//     if (a <= b) return NumbersRes(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
 // Рекурсивный метод.
 Console.WriteLine(NumbersFor(1, 10)); //1 2 3 4 5 6 7 8 9 10
 Console.WriteLine(NumbersRes(1, 10)); //1 2 3 4 5 6 7 8 9 10
