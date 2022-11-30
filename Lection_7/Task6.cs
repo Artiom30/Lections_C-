2786f678959741384f6e35be4953984e68d03162
Artiom30 <mezheynikov1@gmail.com>
2022-11-30 12:32:08 +0300

    Task6

---
+++ Lection_7/Task6.cs
@@ -0,0 +1,17 @@
+// Задача 6: Классы DirectoryInfo and FileInfo[]
+
+
+/*
+string path = "C:/Users/37544/OneDrive/Рабочий стол/C#/Seminars_C_Sharp/Seminars_C#/Seminar_9";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+*/
+
+
